535fce55811ac843147b16065dd5f6be0f84f4e8
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-05-01 17:13:39 +0300

    Update content

---
+++ src/components/TodoApp.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import TodosLogic from "./TodosLogic";
+
+const TodoApp = () => {
+    return (
+      <div>
+        <Header/>
+        <TodosLogic />
+      </div>
+    );
+  };
+  export default TodoApp;
+  
---
+++ src/components/TodosLogic.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import InputTodo from "./InputTodo";
+import TodosList from "./TodosList";
+
+const TodosLogic = () => {
+    const [todos, setTodos] = useState([
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ])
+      const handleChange = (id) => {
+        setTodos((prevState) =>
+          prevState.map((todo) => {
+            if (todo.id === id) {
+              return {
+                ...todo,
+                completed: !todo.completed,
+              };
+            }
+            return todo;
+          })
+        );
+      };
+
+      const delTodo = (id) => {
+        setTodos([
+          ...todos.filter((todo) => {
+            return todo.id !== id;
+          }),
+        ]);
+      };
+
+      const addTodoItem = (title) => {
+        const newTodo = {
+            id: 4,
+            title: title,
+            completed: false,
+          };
+        
+        setTodos([...todos, newTodo])
+      };
+    
+    
+    return (
+        <>
+        <div>
+            <InputTodo addTodoItem={addTodoItem}/>
+            <TodosList 
+            todosProps={todos} 
+            handleChange={handleChange}
+            delTodo={delTodo}
+            />
+      </div>
+        </>
+    )
+  }
+  export default TodosLogic;
+  
